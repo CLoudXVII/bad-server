9459a09501ef34dddfab8b39d1cef740e19ae7fc
CLoudXVII <sharkov.vs@outlook.com>
2025-12-04 13:54:28 +0300

    feat: all TODOs resolved

--- backend/src/controllers/customers.ts
+++ backend/src/controllers/customers.ts
@@ -1,10 +1,11 @@
-import { NextFunction, Request, Response } from 'express'
 import { FilterQuery } from 'mongoose'
-import NotFoundError from '../errors/not-found-error'
+import { NextFunction, Request, Response } from 'express'
+
 import Order from '../models/order'
 import User, { IUser } from '../models/user'
 
-// TODO: Добавить guard admin
+import NotFoundError from '../errors/not-found-error'
+
 // eslint-disable-next-line max-len
 // Get GET /customers?page=2&limit=5&sort=totalAmount&order=desc&registrationDateFrom=2023-01-01&registrationDateTo=2023-12-31&lastOrderDateFrom=2023-01-01&lastOrderDateTo=2023-12-31&totalAmountFrom=100&totalAmountTo=1000&orderCountFrom=1&orderCountTo=10
 export const getCustomers = async (
@@ -153,7 +154,6 @@ export const getCustomers = async (
     }
 }
 
-// TODO: Добавить guard admin
 // Get /customers/:id
 export const getCustomerById = async (
     req: Request,
@@ -171,7 +171,6 @@ export const getCustomerById = async (
     }
 }
 
-// TODO: Добавить guard admin
 // Patch /customers/:id
 export const updateCustomer = async (
     req: Request,
@@ -199,7 +198,6 @@ export const updateCustomer = async (
     }
 }
 
-// TODO: Добавить guard admin
 // Delete /customers/:id
 export const deleteCustomer = async (
     req: Request,
